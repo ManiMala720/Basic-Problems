b7e229afc0c5b9f3832e76390c1b70cae38a9739
Mani Mala <144355017+ManiMala720@users.noreply.github.com>
2025-03-04 20:09:05 +0530

    Update basic1.java

--- basic1.java
+++ basic1.java
@@ -8,6 +8,7 @@ public class basic1 {
     }
     System.out.println("No of digits:"+cnt);
   }
+  
   public static void reverseNumber(int n){
     int rev=0;
     while(n>0){
@@ -17,6 +18,7 @@ public class basic1 {
     }
     System.out.println("Reverse of a number is:"+rev);
   }
+  
   public static void checkPalindrome(int n){
     int original=n;
     int rev=0;
@@ -32,6 +34,7 @@ public class basic1 {
       System.out.println("The given number "+original+" is not a Palindrome");
     }
   }
+  
   public static void armstrongNo(int n){
   String st=String.valueOf(n);  
   int k=st.length();
@@ -49,6 +52,7 @@ public class basic1 {
     System.out.println("The given number "+original+" is not an Armstrong number");
   }
   }
+  
   public static void main(String[] args){
     Scanner sc=new Scanner(System.in);
     System.out.println("Enter a no:");
